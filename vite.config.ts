e5d5ee1eac8d6317fa7c483dba1cae17e1cc357f
Larkin177 <1114673990@qq.com>
2025-11-02 22:10:31 +0800

    11/2 -3

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/jihai-queue/', 
+  base: '/JIHAI-QUEUE/', 
   plugins: [
     vue(),
     vueJsx(),
